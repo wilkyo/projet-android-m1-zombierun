2dd4522c45cd0aacb8ada97670870807d84062a3
jeanbaptiste.perrin38 <jeanbaptiste.perrin38@f01978a2-7eaa-ac77-b40c-0d2e6c96188f>
2012-11-01 14:42:13 +0000

    Ajout de la map et localisation selon le gps

---
+++ trunk/Advanced Zombie Run/src/com/arkwilhow/advzombierun/Marqueur.java
@@ -0,0 +1,53 @@
+package com.arkwilhow.advzombierun;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+
+public class Marqueur extends ItemizedOverlay {
+
+	private ArrayList<OverlayItem> listeMarqueur = new ArrayList<OverlayItem>();
+	private Context mContext;
+	public Marqueur(Drawable arg0) {
+		super(boundCenterBottom(arg0));
+		// TODO Auto-generated constructor stub
+	}
+
+	public Marqueur(Drawable defaultMarker, Context context) {
+		  super(boundCenterBottom(defaultMarker));
+		  mContext = context;
+	}
+	@Override
+	protected OverlayItem createItem(int arg0) {
+		return listeMarqueur.get(arg0);
+	}
+
+	@Override
+	public int size() {
+		return listeMarqueur.size();
+	}
+	
+	public void addMarqueur(OverlayItem item)
+	{
+		listeMarqueur.add(item);
+		populate();
+	}
+
+	@Override
+	protected boolean onTap(int arg0) {
+		// TODO Auto-generated method stub
+		OverlayItem item = listeMarqueur.get(arg0);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+}
