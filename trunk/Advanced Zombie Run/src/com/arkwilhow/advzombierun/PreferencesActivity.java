19d91e3d672ec632f82386f2c08b90d5459ed8b7
wfrancois45 <wfrancois45@f01978a2-7eaa-ac77-b40c-0d2e6c96188f>
2012-11-02 21:53:02 +0000

    Ajout de Spinners pour les listes de choix. Densité et Vitesse. Le nombre et les noms restent à définir entre nous.

--- trunk/Advanced Zombie Run/src/com/arkwilhow/advzombierun/PreferencesActivity.java
+++ trunk/Advanced Zombie Run/src/com/arkwilhow/advzombierun/PreferencesActivity.java
@@ -3,31 +3,67 @@ package com.arkwilhow.advzombierun;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
 
 public class PreferencesActivity extends Activity {
 
-	
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preferences);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_preferences);
 
 		loadPreferences();
-    }
+		setListeners();
+	}
 
 	private void loadPreferences() {
 		SharedPreferences s = getPreferences(MODE_PRIVATE);
-		String mem1 = s.getString("density", "");
-		String mem2 = s.getString("speed", "");
-		//e1.setHint(mem1);
+		((Spinner) findViewById(R.id.spinner_density)).setSelection(s.getInt(
+				"density", 0));
+		((Spinner) findViewById(R.id.spinner_speed)).setSelection(s.getInt(
+				"speed", 0));
 	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_preferences, menu);
-        return true;
-    }
+	private void setListeners() {
+		((Spinner) findViewById(R.id.spinner_density))
+		.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			public void onItemSelected(AdapterView<?> parent,
+					View view, int pos, long id) {
+				SharedPreferences.Editor editor = getPreferences(
+						MODE_PRIVATE).edit();
+				editor.putInt("density", pos);
+				editor.commit();
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+
+		});
+		((Spinner) findViewById(R.id.spinner_speed))
+		.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			public void onItemSelected(AdapterView<?> parent,
+					View view, int pos, long id) {
+				SharedPreferences.Editor editor = getPreferences(
+						MODE_PRIVATE).edit();
+				editor.putInt("speed", pos);
+				editor.commit();
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+
+		});
+}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_preferences, menu);
+		return true;
+	}
 }
