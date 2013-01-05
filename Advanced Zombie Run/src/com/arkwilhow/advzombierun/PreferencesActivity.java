d590e873df4d21b5444227d65ed4f8a43af10a4c
wfrancois45 <wfrancois45@f01978a2-7eaa-ac77-b40c-0d2e6c96188f>
2013-01-05 16:21:16 +0000

    Lancement de Map après choix des préférences. Lancement inactif en multi

--- Advanced Zombie Run/src/com/arkwilhow/advzombierun/PreferencesActivity.java
+++ Advanced Zombie Run/src/com/arkwilhow/advzombierun/PreferencesActivity.java
@@ -2,16 +2,20 @@ package com.arkwilhow.advzombierun;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Spinner;
 
 public class PreferencesActivity extends Activity {
+
+	private static boolean multi;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,76 +34,92 @@ public class PreferencesActivity extends Activity {
 				"speed", 0));
 		((Spinner) findViewById(R.id.spinner_life)).setSelection(s.getInt(
 				"life", 0));
-		((RadioGroup) findViewById(R.id.alertChoices)).check(s.getInt("alert", R.id.alertChoice1));
+		((RadioGroup) findViewById(R.id.alertChoices)).check(s.getInt("alert",
+				R.id.alertChoice1));
 	}
 
 	private void setListeners() {
 		((Spinner) findViewById(R.id.spinner_density))
-		.setOnItemSelectedListener(new OnItemSelectedListener() {
+				.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-			public void onItemSelected(AdapterView<?> parent,
-					View view, int pos, long id) {
-				SharedPreferences.Editor editor = getPreferences(
-						MODE_PRIVATE).edit();
-				editor.putInt("density", pos);
-				editor.commit();
-			}
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int pos, long id) {
+						SharedPreferences.Editor editor = getPreferences(
+								MODE_PRIVATE).edit();
+						editor.putInt("density", pos);
+						editor.commit();
+					}
 
-			public void onNothingSelected(AdapterView<?> arg0) {
-			}
+					public void onNothingSelected(AdapterView<?> arg0) {
+					}
 
-		});
+				});
 		((Spinner) findViewById(R.id.spinner_speed))
-		.setOnItemSelectedListener(new OnItemSelectedListener() {
+				.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-			public void onItemSelected(AdapterView<?> parent,
-					View view, int pos, long id) {
-				SharedPreferences.Editor editor = getPreferences(
-						MODE_PRIVATE).edit();
-				editor.putInt("speed", pos);
-				editor.commit();
-			}
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int pos, long id) {
+						SharedPreferences.Editor editor = getPreferences(
+								MODE_PRIVATE).edit();
+						editor.putInt("speed", pos);
+						editor.commit();
+					}
 
-			public void onNothingSelected(AdapterView<?> arg0) {
-			}
+					public void onNothingSelected(AdapterView<?> arg0) {
+					}
 
-		});
+				});
 		((Spinner) findViewById(R.id.spinner_life))
-		.setOnItemSelectedListener(new OnItemSelectedListener() {
+				.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-			public void onItemSelected(AdapterView<?> parent,
-					View view, int pos, long id) {
-				SharedPreferences.Editor editor = getPreferences(
-						MODE_PRIVATE).edit();
-				editor.putInt("life", pos);
-				editor.commit();
-			}
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int pos, long id) {
+						SharedPreferences.Editor editor = getPreferences(
+								MODE_PRIVATE).edit();
+						editor.putInt("life", pos);
+						editor.commit();
+					}
 
-			public void onNothingSelected(AdapterView<?> arg0) {
-			}
+					public void onNothingSelected(AdapterView<?> arg0) {
+					}
 
-		});
+				});
 
-		((RadioGroup) findViewById(R.id.alertChoices)).setOnCheckedChangeListener(new OnCheckedChangeListener() {
+		((RadioGroup) findViewById(R.id.alertChoices))
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-			public void onCheckedChanged(RadioGroup rg, int checkedId) {
-				System.out.println(checkedId);
-				SharedPreferences.Editor editor = getPreferences(
-						MODE_PRIVATE).edit();
-				editor.putInt("alert", checkedId);
-				editor.commit();
-			}
-			
-		});
-}
+					public void onCheckedChanged(RadioGroup rg, int checkedId) {
+						System.out.println(checkedId);
+						SharedPreferences.Editor editor = getPreferences(
+								MODE_PRIVATE).edit();
+						editor.putInt("alert", checkedId);
+						editor.commit();
+					}
+
+				});
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_preferences, menu);
 		return true;
 	}
-	
+
+	public void run(View v) {
+		if (this.multi) {
+			Toast.makeText(this, "Fonction non encore implémentée", Toast.LENGTH_LONG).show();
+		} else {
+			Intent i = new Intent();
+			i.setClass(this, Map.class);
+			startActivity(i);
+		}
+	}
+
 	public void previous(View v) {
 		finish();
+	}
+	
+	public static void setMulti(boolean multi) {
+		PreferencesActivity.multi = multi;
 	}
 }
